be25faa86c2dfeae20eed9717dc5c8f1eff01615
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-09-16 13:43:03 +0700

    Add files via upload

--- abcscript.js
+++ abcscript.js
@@ -1,28 +1,9 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// JavaScript-onload-fade IN.
+window.onload = function() {
+    var body = document.body;
+    body.classList.add('fade-in'); // Hiển thị trang web dần dần
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
- 
-  slides[slideIndex-1].style.display = "block";
-}
 
 
 
